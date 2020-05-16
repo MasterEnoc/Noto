ca198a770a165f54d489444bd5b32d2245c06fa4
MasterEnoc <brianhernandez222@hotmail.com>
2020-05-15 19:31:46 -0600

    menu constant now is made in other file

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { BrowserWindow, app, Menu, ipcMain} = require('electron');
-const {openFile, openFolder, saveAsFile, emptyJson, saveFile, emptyThisReminder, remindersToTxt, shrinkBar} = require('./scripts/menuFunctions');
 const {stat, writeFile, readFileSync} = require('fs');
+const {menu} = require('./menu.js');
 
 global.win;
 global.currentPath = '';
@@ -18,42 +18,6 @@ try {
 } catch (error) {
 
 }
-
-const menu = [
-    {
-        label: '&File', submenu: [
-            {label: '&Open File', click:()=>{openFile();}, accelerator:'Ctrl+o'},
-            {label: 'O&pen Folder', click: ()=>{openFolder();}, accelerator:'Ctrl+Shift+o'},
-            {label: '&Save As', click: ()=>{saveAsFile();}, accelerator: 'Ctrl+Shift+s'},
-            {label: 'S&ave', click: ()=>{saveFile();}, accelerator:'Ctrl+s'}
-        ]
-    },
-    { label: '&Edit', submenu:[
-        {label:'Undo', role:'undo'},
-        {label:'Redo', role:'redo'},
-        {label:'Copy', role:'copy'},
-        {label:'Paste', role:'paste'},
-        {label:'Cut', role:'cut'},
-        {label:'Move left', accelerator:'Ctrl+h'},
-        {label:'Move down', accelerator:'Ctrl+j'},
-        {label:'Move up', accelerator:'Ctrl+k'},
-        {label:'Move right', accelerator:'Ctrl+l'}
-    ]},
-    {
-        label: '&Settings', submenu: [
-            { label: 'Quit', role: 'quit' },
-            { label: 'Reload Noto', role: 'reload' },
-            { label: 'Dev Tools', role: 'toggleDevTools' },
-            { label: 'Toggle file browser',click: ()=>{win.webContents.send('shrink-bar');}, accelerator: 'Ctrl+b'}
-        ]
-    },
-    { label: '&Reminders', submenu: [
-        {label: 'Empty reminders', click:()=>{emptyJson();}},
-        {label:'Empty this reminder', click:()=>{emptyThisReminder();}},
-        {label:'Reminders to txt', click:()=>{remindersToTxt();}}
-        ]
-    }
-];
 
 Menu.setApplicationMenu(Menu.buildFromTemplate(menu));
 
